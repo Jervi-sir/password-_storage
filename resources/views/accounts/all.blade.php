4c7203bf74545ce7987a321c897ccdcd4a086433
Gacem Bekhira <gacembekhira@gmail.com>
2021-06-29 19:50:35 +0100

    edited display none of modal

--- resources/views/accounts/all.blade.php
+++ resources/views/accounts/all.blade.php
@@ -9,6 +9,25 @@ foreach ($social_used as $social)
 }
 
 ?>
+
+
+<style>
+    /* The Modal (background) */
+    .modal {
+      display: none; /* Hidden by default */
+      position: fixed; /* Stay in place */
+      z-index: 1; /* Sit on top */
+      padding-top: 100px; /* Location of the box */
+      left: 0;
+      top: 0;
+      width: 100%; /* Full width */
+      height: 100%; /* Full height */
+      overflow: auto; /* Enable scroll if needed */
+      background-color: rgb(0,0,0); /* Fallback color */
+      background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    }
+</style>
+
     @if (session('alert'))
     <div class="bg-green-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
         <strong class="font-bold">{{ session('alert') }}</strong>
@@ -137,22 +156,6 @@ foreach ($social_used as $social)
             </div>
         </div>
 
-        <style>
-            /* The Modal (background) */
-            .modal {
-              display: none; /* Hidden by default */
-              position: fixed; /* Stay in place */
-              z-index: 1; /* Sit on top */
-              padding-top: 100px; /* Location of the box */
-              left: 0;
-              top: 0;
-              width: 100%; /* Full width */
-              height: 100%; /* Full height */
-              overflow: auto; /* Enable scroll if needed */
-              background-color: rgb(0,0,0); /* Fallback color */
-              background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-            }
-        </style>
 
 <script>
     var modals = document.getElementsByClassName('modal');
