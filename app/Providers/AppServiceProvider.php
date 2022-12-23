f381c4cac382f336e4b0eacf10e546419c0372d5
jervi <gacembekhira@gmail.com>
2022-12-23 21:57:08 +0100

    fresh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultstringLength(191);
-        URL::forceScheme('https');
-
+        //
     }
 }
