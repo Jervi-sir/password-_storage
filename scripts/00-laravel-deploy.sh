9f7fcebd7cd24239a6e497e125fb77edc6c45a35
jervi <gacembekhira@gmail.com>
2022-12-23 23:48:33 +0100

    not ignoring public/build

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -13,4 +13,6 @@ php artisan route:cache
 echo "Running migrations..."
 #php artisan migrate:fresh --force
 
+npm install --save-dev vite laravel-vite-plugin
+npm install --save-dev @vitejs/plugin-vue
 npm run build
